f03e52c444fde72eeacfd043c022ce84b5e8e8d9
snagasuri <ramnag2003@gmail.com>
2024-06-04 10:35:31 -0700

    removed secrets

--- gpt-server.py
+++ gpt-server.py
@@ -1,7 +1,7 @@
 import anthropic
 client = anthropic.Anthropic(
     # defaults to os.environ.get("ANTHROPIC_API_KEY")
-    api_key="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",
+    api_key="",
 )
 message = client.messages.create(
     model="claude-3-opus-20240229",
